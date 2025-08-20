c86084d70e4ab015d90b3b4dbcc6ea560f86157d
tanimsk <sktanim5800@gmail.com>
2025-08-20 11:22:14 +0600

    updated

--- server/administrator/views.py
+++ server/administrator/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import BasePermission
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from django.http import HttpResponse
+from django.utils import timezone
+from django.conf import settings
+from django.db.models import Q
 
-# Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 500
+    page_query_param = "p"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "page_size": self.get_page_size(self.request),
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "num_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "results": data,
+            }
+        )
